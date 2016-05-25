3adeab230f15a82d2c7f0601f43a4a3cabca3c0c
Will Johnson <trashmoldwilliams@gmail.com>
2016-05-25 15:27:27 -0700

    Add failsafe to entering decimals and negative numbers.

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,29 @@
 console.log("To make a pyramid, type 'makePyramid(x)' into the console, with 'x' representing the integer value of rows the pyramid will have.");
 
 var makePyramid = function(input) {
-	var output = "\n";
+	var output = "";
 
-	for(var i = 0; i < input; i++) {
-
-		//Add left margin
-		for(var j = input - 2 - i; j >= 0; j--) {
-			output += " ";
-		}
-
-		//Fill out row
-		output += "x";
-		for(var j = i; j > 0; j--) {
-			output += " x";
-		}
-
-		//End line
+	if(input % 1 != 0 || input < 0) {
+		output = "Enter a positive integer, please."
+	} else {
 		output += "\n";
+
+		for(var i = 0; i < input; i++) {
+
+			//Add left margin
+			for(var j = input - 2 - i; j >= 0; j--) {
+				output += " ";
+			}
+
+			//Fill out row
+			output += "x";
+			for(var j = i; j > 0; j--) {
+				output += " x";
+			}
+
+			//End line
+			output += "\n";
+		}
 	}
 
 	return output;
