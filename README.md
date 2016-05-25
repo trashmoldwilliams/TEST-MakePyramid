dddc6010d27c78629c44df4a50b1d7dcaa4c3c0f
Will Johnson <trashmoldwilliams@gmail.com>
2016-05-25 15:20:29 -0700

    Add page instructions and README.

---
+++ README.md
@@ -0,0 +1,9 @@
+# Make Pyramid
+
+This application was created for a challenge from Hubb (http://www.hubb.me/), to create an app that outputs a pyramid with the amount of rows of the user's integer input.
+
+## Instructions
+* Clone project and open index.html in browser
+
+## Technolgies Used
+* Javascript
--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,22 @@
-console.log("Instructions");
+console.log("To make a pyramid, type 'makePyramid(x)' into the console, with 'x' representing the integer value of rows the pyramid will have.");
 
 var makePyramid = function(input) {
 	var output = "\n";
 
 	for(var i = 0; i < input; i++) {
+
+		//Add left margin
 		for(var j = input - 2 - i; j >= 0; j--) {
 			output += " ";
 		}
 
+		//Fill out row
 		output += "x";
-
 		for(var j = i; j > 0; j--) {
 			output += " x";
 		}
 
+		//End line
 		output += "\n";
 	}
 
